396479cd373a7ef2acb018f750c7779f26253ac1
Samiksha-Hade <100122830+Samiksha-Hade@users.noreply.github.com>
2022-03-09 17:25:17 +0530

    Add files via upload

---
+++ empassignment/empassignment/empassignment/src/main/resources/data.sql
@@ -0,0 +1,25 @@
+insert into employee(id, name, last_name) 
+values(100,'Samiksha','Sam');
+insert into employee (id, name, last_name) 
+values(101,'Ishwari' ,'Ishu' );
+insert into employee(id, name, last_name ) 
+values(102,'Mrunal', 'Mru');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
